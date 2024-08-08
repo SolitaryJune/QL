2953ad3b3004a2e90c82def788a8bc8780161c8a
24 <39792363+SolitaryJune@users.noreply.github.com>
2024-08-08 17:19:27 +0800

    Update lang1.sh

--- lang1.sh
+++ lang1.sh
@@ -356,7 +356,7 @@ docker run -dit \
   --name qinglong \
   --hostname qinglong \
   --restart always \
-  whyour/qinglong:2.10.13
+  btf6gsjy.mirror.aliyuncs.com/whyour/qinglong:2.10.13
   
   docker restart qinglong > /dev/null 2>&1
   sleep 2

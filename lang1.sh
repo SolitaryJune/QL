0dd1bcd47a4f9bd6fb34a25f3ecb8dd99e6ae645
24 <39792363+SolitaryJune@users.noreply.github.com>
2024-09-01 00:30:48 +0800

    Update lang1.sh

--- lang1.sh
+++ lang1.sh
@@ -356,7 +356,7 @@ docker run -dit \
   --name qinglong \
   --hostname qinglong \
   --restart always \
-  m.daocloud.io/docker.io/whyour/qinglong:2.17.9
+  m.daocloud.io/docker.io/whyour/qinglong:debian
   
   docker restart qinglong > /dev/null 2>&1
   sleep 2
